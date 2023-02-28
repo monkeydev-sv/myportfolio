1d3eb2f926fbbfe1e2adc1ad8c04d84a82c54f17
Christer Holm <christer@holmdeveloper>
2023-02-28 09:03:54 +0100

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import './App.css';
 function App() {
   return (
 <>
+<div className="app">
 <Router>
 <Navbar />
    <Routes>
@@ -16,7 +17,7 @@ function App() {
 <Footer/>
    </Router>
 
-   
+   </div>
 </>   
      
  
