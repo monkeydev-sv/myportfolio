236f92dff2fd4a330004165eda235e13bfa141d7
Christer Holm <christer@holmdeveloper>
2023-03-04 16:08:02 +0100

    first

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,22 @@
 import React from "react";
 import "./footer.css";
-import { FiInstagram, FiGithub,FiFacebook } from "react-icons/fi";
+import { FiInstagram, FiGithub, FiFacebook } from "react-icons/fi";
 const Footer = () => {
   return (
     <div>
-  
-      
       <br />
       Holm Developer © 2023. Alla rättigheter förbehållna.
       <br />
-      
       <a href="https://www.instagram.com/holmdev/">
-        <FiInstagram size={32} className="social"/>
+        <FiInstagram size={32} className="social" />
       </a>
-      
       <a href="https://github.com/holmdevelopers">
-        <FiGithub size={32} className="social"/>
-        </a>
-        <a href="https://www.facebook.com/holmdeveloper/">
-        <FiFacebook size={32} className="social"/>
-     </a>
-     </div>
-
+        <FiGithub size={32} className="social" />
+      </a>
+      <a href="https://www.facebook.com/holmdeveloper/">
+        <FiFacebook size={32} className="social" />
+      </a>
+    </div>
   );
 };
 export default Footer;
