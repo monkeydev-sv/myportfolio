bf8e4742aeda54772d2759dd0eec555ff3dff716
Christer Holm <christer@holmdeveloper>
2023-03-10 13:10:44 +0100

    some changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import logostor from "../assets/logostor.png";
 import { Link } from "react-router-dom";
+import { RiMenu5Line } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+
 import "./navbar.css";
 const Navbar = () => {
   const [isActive, setisActive] = React.useState(false);
   return (
     <nav class="navbar" role="navigation" aria-label="main navigation">
-      <a href="/">
-        <img src={logostor} height="4px" />
-      </a>
+     
 
       <a
         onClick={() => {
@@ -19,6 +20,7 @@ const Navbar = () => {
         aria-label="menu"
         aria-expanded="false"
         data-target="navbar"
+        
       >
         <div
           class="menu"
@@ -26,36 +28,15 @@ const Navbar = () => {
           aria-label="Main Menu"
         >
           {isActive ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              fill="currentColor"
-              class="bi bi-x"
-              viewBox="0 0 16 16"
-            >
-              {" "}
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />{" "}
-            </svg>
+           <AiOutlineClose className="close"/>
           ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48 "
-              fill="currentColor"
-              class="bi bi-justify"
-              viewBox="0 0 16 16"
-            >
-              {" "}
-              <path
-                fill-rule="evenodd"
-                d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
-              />{" "}
-            </svg>
+           <RiMenu5Line className="bar"/>
           )}
         </div>
       </a>
-
+      <a href="/">
+        <img src={logostor} />
+      </a>
       <div
         id="navbarBar"
         class="navbar-menu"
