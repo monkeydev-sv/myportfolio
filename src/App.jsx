b72b2cd6e91bcd232f3ea66c2f1ddd06ed8b959b
Christer Holm <christer@holmdeveloper>
2023-03-14 11:39:46 +0100

    In to bitbucket

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import About from './pages/about/index';
 import Case from './pages/case/index';
 import Footer from './components/footer';
 import Notification from './components/notification';
+import { BiCookie } from "react-icons/bi";
 
+import CookieConsent, { Cookies } from "react-cookie-consent";
 import './App.css';
 
 
@@ -28,7 +30,37 @@ function App() {
   
 
    </Routes>
+
 <Footer/>
+<CookieConsent
+      location="bottom"
+      buttonText="Okej!"
+      cookieName="CookieMonster"
+      style={{ background: "#56aee0", color: "#000000", fontSize: "18px" }}
+     
+      buttonStyle={{
+        background: "#000000",
+        color: "#56aee0",
+        border:"white",
+        fontSize: "15px",
+      }}
+      expires={150}
+    >
+     
+        <div className="columns ">
+          <div class="column">
+            <div className="cookie">
+              <BiCookie size={42} />
+            </div>
+          </div>
+          <div class="column">
+            <div className="textCookie">
+            <strong>Men först, Cookies</strong><br/>
+            Vi använder viktiga cookies för att få Holm Developer att fungera. Vi Vill att din upplevelse ska bli så bra som möjligt!</div>
+          </div>
+        </div>
+  
+    </CookieConsent>
    </Router>
 
    </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,43 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "bulma/css/bulma.css";
-import { BiCookie } from "react-icons/bi";
 
-import CookieConsent, { Cookies } from "react-cookie-consent";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
 <React.StrictMode>
 
-    <CookieConsent
-      location="bottom"
-      buttonText="Okej!"
-      cookieName="CookieMonster"
-      style={{ background: "#56aee0", color: "#000000", fontSize: "18px" }}
-      buttonStyle={{
-        background: "#000000",
-        color: "#56aee0",
-        border:"white",
-        fontSize: "15px",
-      }}
-      expires={150}
-    >
-      <div className="container">
-        <div className="columns ">
-          <div class="column">
-            <div className="cookie">
-              <BiCookie size={48} />
-            </div>
-          </div>
-          <div class="column">
-            <div className="textCookie">
-              Denna sidan använder kakor för att du ska få den bästa
-              upplevelsen!{" "}
-            </div>
-          </div>
-        </div>
-      </div>
-    </CookieConsent>
+    
     <App />
   </React.StrictMode>
 );
